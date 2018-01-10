d378803707ec2a5057a0bd6caa60a285fba445da
Arif Dzikrullah <ardzyix@gmail.com>
2018-01-11 00:43:56 +0700

    added migrations init

---
+++ apps/migrations/__init__.py
@@ -0,0 +1,12 @@
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# 
+# Author: Arif Dzikrullah
+#         ardzix@hotmail.com
+# 
+# File Created: Wednesday, 10th January 2018 11:24:54 pm
+# Last Modified: Wednesday, 10th January 2018 11:38:35 pm
+# Modified By: Arif Dzikrullah (ardzix@hotmail.com)
+# 
+# Give the best to the world
+# Copyright - 2018 Ardz.Co
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
