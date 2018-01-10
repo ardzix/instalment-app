5aa8f10e0e56544c2c694754f6e02bccf9ea3dd6
Arif Dzikrullah <ardzyix@gmail.com>
2018-01-11 00:48:37 +0700

    moving files

---
+++ apps/tests.py/__init__.py
@@ -0,0 +1,15 @@
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# 
+# Author: Arif Dzikrullah
+#         ardzix@hotmail.com
+# 
+# File Created: Thursday, 11th January 2018 12:45:51 am
+# Last Modified: Thursday, 11th January 2018 12:45:54 am
+# Modified By: Arif Dzikrullah (ardzix@hotmail.com)
+# 
+# Give the best to the world
+# Copyright - 2018 Ardz.Co
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+from django.test import TestCase
+
+# Create your tests here.
